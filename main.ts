9d6b46175961708ac7980b98e40bb262c6fca3a7
Nithesh Krishna <79267358+NitheshKrishna2020@users.noreply.github.com>
2021-12-04 22:06:21 +0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,13 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showString("\"I am Nithesh from Villa College!\"")
+})
+basic.showString("Hello!")
+basic.forever(function () {
+    basic.showLeds(`
+        # # . # #
+        # . # . #
+        # . . . #
+        . # . # .
+        . . # . .
+        `)
+})
